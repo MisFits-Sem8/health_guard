c9a72fccfc80fd1c0b6822b538d32270127538bc
KumuthuA <kumuthua@gmail.com>
2024-04-17 04:14:51 +0530

    16- signout button added

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:health_app/common/color_extension.dart';
+import 'package:health_app/common_widgets/rounded_btn.dart';
+import 'package:health_app/services/auth_service.dart';
+
+import '../../common/color_extension.dart';
+import '../login/login.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -9,10 +13,36 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  final AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: TColour.white,
+      body: Align(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Welcome!",
+              style: TextStyle(
+                  color: TColour.black1,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w700),
+            ),
+            RoundedButton(
+              onPressed: () async {
+                _auth.signOut;
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LoginView(),
+                    ));
+              },
+              title: 'Sign Out',
+            )
+          ],
+        ),
+      ),
     );
   }
 }
