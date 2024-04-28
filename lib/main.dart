4ada30ba18ebe27dc4dcd292846921bcc39b58b7
harshani-tools4ai <harshani.19@cse.mrt.ac.lk>
2024-04-28 14:42:41 +0530

    issue fixed

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class MyApp extends StatelessWidget {
           fontFamily: "Poppins",
           useMaterial3: true,
         ),
-        home:const SleepTrackerView());
-        // home: const GettingStartedView());
+        // home:const SleepTrackerView());
+        home: const GettingStartedView());
   }
 }
